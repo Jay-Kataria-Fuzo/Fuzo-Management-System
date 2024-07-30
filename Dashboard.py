6c6e87c8b857571ddd8fc5d3745b7c8fd757442a
jay_kataria <jay_kataria@joinfuzo.com>
2024-07-31 00:55:47 +0530

    edit brand

--- Dashboard.py
+++ Dashboard.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.set_page_config("Management Dashboard", "https://joinfuzo.com/images/favicon/favicon-32x32.png", layout="wide")
+
+st.image("https://joinfuzo.com/images/logo-fuzo.png")
+st.header("Management System")
--- pages/Brand_Master.py
+++ pages/Brand_Master.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from st_supabase_connection import SupabaseConnection, execute_query
+import pandas as pd
 
 st.set_page_config("Management Dashboard", "https://joinfuzo.com/images/favicon/favicon-32x32.png", layout="wide")
 
@@ -34,4 +35,25 @@ if submit_add:
         placeholder_form_top.error("Fill all required Fields!")
         placeholder_form_bottom.error("Fill all required Fields!")
 
+df_brands = pd.DataFrame(execute_query(conn.table("brands").select("*", count="None"), ttl="0").data)
+df_brands["identifier"] = df_brands["id"].astype(str)+" - "+df_brands["brand_name"]
+sel_brand = tab2.selectbox("Select Brand", options=list(df_brands["identifier"]), index=None)
 
+if sel_brand:
+    with tab2.form("Edit Brand"):
+        placeholder_form_top = st.empty()
+        new_brand_name = st.text_input("Brand Name*", value=df_brands.loc[df_brands["identifier"] == sel_brand, "brand_name"].values[0])
+        new_brand_image = st.text_input("Brand Image", value=df_brands.loc[df_brands["identifier"] == sel_brand, "brand_image"].values[0])
+        col_submit, col_refresh = st.columns(2)
+        submit_edit = col_submit.form_submit_button("Save", use_container_width=True)
+        reset_edit = col_refresh.form_submit_button("Refresh", type="primary", use_container_width=True )
+        placeholder_form_bottom = st.empty()
+
+    if submit_edit:
+        if new_brand_name.strip() != "":
+            placeholder_form_top.success("Brand updated Successfully!")
+            placeholder_form_bottom.success("Brand updated Successfully!")
+            execute_query(conn.table("brands").update([{"brand_name":new_brand_name,"brand_image":new_brand_image}], count="None").eq("id",df_brands.loc[df_brands["identifier"] == sel_brand, "id"].values[0] ), ttl='0')
+        else:
+            placeholder_form_top.error("Fill all required Fields!")
+            placeholder_form_bottom.error("Fill all required Fields!")
